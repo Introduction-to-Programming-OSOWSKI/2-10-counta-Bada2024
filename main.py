f3cc2c4925382db959aca7c74a72a12ed32bad72
Bada2024 <830561@mystma.org>
2021-11-04 22:54:56 -0500

    2nd at home

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    c=0
+    for i in range(0,len(w)):
+        if w[i] == "a":
+             c=c+1
+    return c
+print(countA("aaaaaaaaaaaaaaaaaaaa"))
